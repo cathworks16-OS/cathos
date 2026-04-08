7b8edff5ba38d0b58e438a997999381edddaa78c
cathworks16-OS <cathworks16@gmail.com>
2026-04-08 18:38:39 +0800

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,13 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2868
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww14360\viewh16460\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 import \{ createClient \} from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'\
+\
+const supabaseUrl = 'YOUR_SUPABASE_URL'\
+const supabaseKey = 'YOUR_SUPABASE_ANON_KEY'\
+\
+export const supabase = createClient(supabaseUrl, supabaseKey)}
